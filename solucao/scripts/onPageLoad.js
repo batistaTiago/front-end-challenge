96004bb82ac5bf6b7127703e55693d2d19dd43c0
batistaTiago <ekyidag@gmail.com>
2019-05-15 22:28:32 -0300

    segundo dia

---
+++ solucao/scripts/onPageLoad.js
@@ -0,0 +1,122 @@
+let fadeDuration = 450
+
+$(document).ready(
+    () => {
+
+        $('#selectVersao').on(
+            'change',
+            (e) => {
+                e.target.blur()
+            }
+        )
+
+        // $('#menu-telefones li a').on(
+        //     'click',
+        //     (e) => {
+        //         $('.lista-telefones').toggleClass('scaleY-0')
+        //         $('.lista-telefones').toggleClass('scaleY-1')
+        //         $('.lista-telefones li').toggleClass('d-none')
+        //     }
+        // )
+
+        // $(window).on(
+        //     'scroll',
+        //     () => {
+        //         if (window.scrollY > 25) {
+        //             $('header').addClass('fixed-top py-2 bg-dark')
+        //         } else {
+        //             $('header').removeClass('fixed-top py-2 bg-dark')
+        //         }
+        //     }
+        // )
+
+
+        $('#versoes button').on(
+            'click',
+            (e) => {
+                $('button.active').removeClass('active')
+                $('#' + e.target.id).addClass('active')
+
+                let idCarro = e.target.id.replace('button', '')
+
+                $.get(
+                    'http://localhost:3000/carros?nome=' + idCarro,
+                    (data, status) => {
+                        if ((status === 'success') && (data.length === 1)) {
+                            let dadosCarro = data[0]
+                            if (dadosValidos(dadosCarro)) {
+                                atualizarImagem(dadosCarro.featured_image)
+                                preencherTabela(dadosCarro)
+                                atualizarItensDeSerie(dadosCarro.itens_de_serie)
+                            }
+                        }
+                        else {
+                            alert('houve um erro');
+                        }
+                    }
+                )
+            }
+        )
+    }
+)
+
+function dadosValidos(dados) {
+    return (
+        dados.featured_image &&
+        dados.cilindros_valvulas &&
+        dados.diametro_curso &&
+        dados.cilindradas &&
+        dados.potencia_ecc_cv &&
+        dados.potencia_rev_min &&
+        dados.torque_maximo &&
+        dados.torque_rev_min &&
+        dados.itens_de_serie &&
+        dados.itens_de_serie.length > 0
+    )
+}
+
+function atualizarImagem(url) {
+    $(".imagem-carro").fadeOut(
+        fadeDuration,
+        () => {
+            $('.imagem-carro').attr('src', url)
+            $(".imagem-carro").fadeIn(fadeDuration)
+        }
+    )
+}
+
+function preencherTabela(dados) {
+    $("#tabela-infos").fadeOut(
+        fadeDuration,
+        () => {
+            $("#cilindros_valvulas").html(dados.cilindros_valvulas)
+            $("#diametro_curso").html(dados.diametro_curso)
+            $("#cilindradas").html(dados.cilindradas)
+            $("#potencia_ecc_cv").html(dados.potencia_ecc_cv)
+            $("#potencia_rev_min").html(dados.potencia_rev_min)
+            $("#torque_maximo").html(dados.torque_maximo)
+            $('#torque_rev_min').html(dados.torque_rev_min)
+
+            $("#tabela-infos").fadeIn(fadeDuration)
+        }
+    )
+}
+
+function atualizarItensDeSerie(dados) {
+    $('#itens-serie-container').fadeOut(
+        fadeDuration,
+        () => {
+            $('#itens-serie-container').html('')
+
+            for (let item of dados) {
+                let itemHTMLElement = document.createElement('li')
+                itemHTMLElement.className = 'item-serie'
+                itemHTMLElement.innerHTML = item
+                $('#itens-serie-container').append(itemHTMLElement)
+            }
+
+            $('#itens-serie-container').fadeIn(fadeDuration)
+        }
+    )
+
+}
